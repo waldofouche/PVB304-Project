834e71c1bc26eff174624e46a7301a929efaae70
Waldo Fouche <waldofouche1@gmail.com>
2020-09-17 00:42:58 +1000

    Update Generated_signal_plot.m

--- Generated_signal_plot.m
+++ Generated_signal_plot.m
@@ -21,3 +21,6 @@ release(spectrum);
 
 %.Mat to .Wav might be usefull
 % https://www.mathworks.com/matlabcentral/answers/306487-convert-mat-files-to-wav
+
+% Open .mat in python
+% https://www.mathworks.com/matlabcentral/answers/383427-proper-importing-of-matlab-structures-into-python
