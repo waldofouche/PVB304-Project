7bce874ba4a2695d13d58c9a25c2f27d877c6769
Waldo Fouche <waldofouche1@gmail.com>
2020-09-16 23:21:21 +1000

    Create signal_tf.py

---
+++ signal_tf.py
@@ -0,0 +1,12 @@
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+class_names = ['5G', '4G LTE', '3G', '2G']
+
